920bc2f8ceb6b3ee993e35c7b38398a973bb8cb0
Rubén Chávez <luisrubenchavez@gmail.com>
2021-02-07 18:54:22 -0700

    Fixing app.R

--- Proyecto/shiny/shiny2/app.R
+++ Proyecto/shiny/shiny2/app.R
@@ -70,7 +70,16 @@ ui =
 
             tags$div(class="container float-none",
                      tabItems(
-                         tabItem(tabName = "analisisD",h2("Analisis Exploratorio Dinamico")),
+                         tabItem(tabName = "analisisD",h2("Analisis Exploratorio Dinamico"),
+                                 fluidRow(
+                                     titlePanel(h3("Gráficos de dispersión")),
+                                     selectInput("f", "Selecciona el valor de x",
+                                                 choices = names(table(dataServer$tematica_1))),
+                                     
+                                     box(plotOutput("output_plot", height = 300, width = 460) )
+                                     
+                                 )
+                                 ),
                          
                          tabItem(tabName = "analisisE",h2("Analisis Exploratorio Estatico"),br(),
                                  img( src = "EdadMujeresReport.png"),br(),
@@ -79,7 +88,24 @@ ui =
                                  img( src = "scatterCivilOcupacion.png"),br(),
                                  img( src = "ViolenciaMDateCuarentena.png")),
                          
-                         tabItem(tabName = "bayesiano",h2("Contraste de Hipotesis Bayesiano")),
+                         tabItem(tabName = "bayesiano",h2("Contraste de Hipotesis Bayesiano"),
+                                 fluidRow(
+                                     titlePanel(h3("Justificación de variables tomadas para el clasificador Bayesiano")),
+                                     
+                                     p(column(4, offset = 0.1,
+                                              'En el resumen se mencionó que las variables que iban a ser consideradas para
+                            predecir el sexo del usuario eran la ocupación y la temática. para justificar
+                            esta elección basta observar que existe una gran disparidad de género si hacemos
+                            referencia a la ocupación o a la temática como se muestra a continuación
+                            ')),
+                                     br(offset = 0.1,img( src = "LlamadasOcupacionSexo.png", 
+                                                          height = 550, width = 1100)),
+                                     
+                                     h3("  Frecuencia de llamadas por temática"),
+                                     br(offset = 0.1,img( src = "FrecuanciaTematicaSexo.png", 
+                                                          height = 550, width = 1100))
+                                 )
+                                 ),
                          
                          tabItem(tabName = "redes",h2("Contraste de Hipotesis Redes Neuronales")),
                          
@@ -90,100 +116,11 @@ ui =
                          tabItem(tabName = "series",h2("Series de Tiempo")),
                          
                          tabItem(tabName = "Conclusiones",h2("Imagenes de resultados y conclusiones"))
-                     )),
-            tabItems(
-                tabItem(tabName = "analisisD",h2("Analisis Exploratorio Dinamico"),
-                        fluidRow(
-                            titlePanel(h3("Gráficos de dispersión")),
-                            selectInput("f", "Selecciona el valor de x",
-                                        choices = names(table(dataServer$tematica_1))),
-                            
-                            box(plotOutput("output_plot", height = 300, width = 460) )
-                            
-                        )
-                        ),
-                
-                tabItem(tabName = "analisisE",h2("Analisis Exploratorio Estatico")),
-                
-                tabItem(tabName = "bayesiano",h2("Contraste de Hipotesis Bayesiano"),
-                        fluidRow(
-                            titlePanel(h3("Justificación de variables tomadas para el clasificador Bayesiano")),
-                            
-                            p(column(4, offset = 0.1,
-                            'En el resumen se mencionó que las variables que iban a ser consideradas para
-                            predecir el sexo del usuario eran la ocupación y la temática. para justificar
-                            esta elección basta observar que existe una gran disparidad de género si hacemos
-                            referencia a la ocupación o a la temática como se muestra a continuación
-                            ')),
-                            br(offset = 0.1,img( src = "LlamadasOcupacionSexo.png", 
-                                                 height = 550, width = 1100)),
-                            
-                            h3("  Frecuencia de llamadas por temática"),
-                            br(offset = 0.1,img( src = "FrecuanciaTematicaSexo.png", 
-                                                 height = 550, width = 1100))
-                            )
-                        ),
-                
-                tabItem(tabName = "redes",h2("Contraste de Hipotesis Redes Neuronales")),
-                
-                tabItem(tabName = "rn",h2("Redes Neuronales")),
-                
-                tabItem(tabName = "regresion",h2("Regresion Lineal")),
-                
-                tabItem(tabName = "series",h2("Series de Tiempo")),
-                
-                tabItem(tabName = "Conclusiones",h2("Imagenes de resultados y conclusiones"))
-            )
-            
-
-            tabItems(
-                tabItem(tabName = "analisisD",h2("Analisis Exploratorio Dinamico"),
-                        fluidRow(
-                            titlePanel(h3("Gráficos de dispersión")),
-                            selectInput("f", "Selecciona el valor de x",
-                                        choices = names(table(dataServer$tematica_1))),
-                            
-                            box(plotOutput("output_plot", height = 300, width = 460) )
-                            
-                        )
-                        ),
-                
-                tabItem(tabName = "analisisE",h2("Analisis Exploratorio Estatico")),
-                
-                tabItem(tabName = "bayesiano",h2("Contraste de Hipotesis Bayesiano"),
-                        fluidRow(
-                            titlePanel(h3("Justificación de variables tomadas para el clasificador Bayesiano")),
-                            
-                            p(column(4, offset = 0.1,
-                            'En el resumen se mencionó que las variables que iban a ser consideradas para
-predecir el sexo del usuario eran la ocupación y la temática. para justificar
-esta elección basta observar que existe una gran disparidad de género si hacemos
-referencia a la ocupación o a la temática como se muestra a continuación
-')),
-                            br(offset = 0.1,img( src = "LlamadasOcupacionSexo.png", 
-                                                 height = 550, width = 1100)),
-                            
-                            h3("  Frecuencia de llamadas por temática"),
-                            br(offset = 0.1,img( src = "FrecuanciaTematicaSexo.png", 
-                                                 height = 550, width = 1100))
-                            )
-                        ),
-                
-                tabItem(tabName = "redes",h2("Contraste de Hipotesis Redes Neuronales")),
-                
-                tabItem(tabName = "rn",h2("Redes Neuronales")),
-                
-                tabItem(tabName = "regresion",h2("Regresion Lineal")),
-                
-                tabItem(tabName = "series",h2("Series de Tiempo")),
-                
-                tabItem(tabName = "Conclusiones",h2("Imagenes de resultados y conclusiones"))
-            )
-
-            
+                     ))
+                )
         ) 
         
-    )
+    
 
 # Run the application 
 shinyApp(ui = ui, server = server)
