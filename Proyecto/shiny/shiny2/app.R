99b1cc7ceb4a5e78b867182e8785bbc524d7eca8
Rubén Chávez <luisrubenchavez@gmail.com>
2021-02-07 18:17:31 -0700

    Added Dynamic Scatter and Bayes

--- Proyecto/shiny/shiny2/app.R
+++ Proyecto/shiny/shiny2/app.R
@@ -9,7 +9,9 @@
 
 library(shiny)
 library(shinydashboard)
-
+library(ggplot2)
+library(dplyr)
+dataServer<-read.csv('www/DataLM1.csv')
 
 # Define server logic required to draw a histogram
 server <- function(input, output,session) {
@@ -25,6 +27,22 @@ server <- function(input, output,session) {
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
+    output$output_plot <- renderPlot({ 
+        
+        datafiltered <- filter(dataServer,tematica_1==input$f)
+        
+        horaFr <- table(datafiltered$hora_alta)
+        horaFr <- as.data.frame(horaFr)
+        
+        horaFr$Var1 <- as.numeric(as.character(horaFr$Var1))
+        horaFr$Freq <- as.numeric(as.character(horaFr$Freq))
+        
+        ggplot(horaFr, aes(x =Var1, y =Freq)) +
+            geom_point() +
+            ylab('No. casos') +
+            xlab('Hora de alta')
+        
+    }) 
 }
 
 
@@ -50,11 +68,37 @@ ui =
             tags$b(h1("Analisis de lineas de soporte en Mexico"))
             ,
             tabItems(
-                tabItem(tabName = "analisisD",h2("Analisis Exploratorio Dinamico")),
+                tabItem(tabName = "analisisD",h2("Analisis Exploratorio Dinamico"),
+                        fluidRow(
+                            titlePanel(h3("Gráficos de dispersión")),
+                            selectInput("f", "Selecciona el valor de x",
+                                        choices = names(table(dataServer$tematica_1))),
+                            
+                            box(plotOutput("output_plot", height = 300, width = 460) )
+                            
+                        )
+                        ),
                 
                 tabItem(tabName = "analisisE",h2("Analisis Exploratorio Estatico")),
                 
-                tabItem(tabName = "bayesiano",h2("Contraste de Hipotesis Bayesiano")),
+                tabItem(tabName = "bayesiano",h2("Contraste de Hipotesis Bayesiano"),
+                        fluidRow(
+                            titlePanel(h3("Justificación de variables tomadas para el clasificador Bayesiano")),
+                            
+                            p(column(4, offset = 0.1,
+                            'En el resumen se mencionó que las variables que iban a ser consideradas para
+predecir el sexo del usuario eran la ocupación y la temática. para justificar
+esta elección basta observar que existe una gran disparidad de género si hacemos
+referencia a la ocupación o a la temática como se muestra a continuación
+')),
+                            br(offset = 0.1,img( src = "LlamadasOcupacionSexo.png", 
+                                                 height = 550, width = 1100)),
+                            
+                            h3("  Frecuencia de llamadas por temática"),
+                            br(offset = 0.1,img( src = "FrecuanciaTematicaSexo.png", 
+                                                 height = 550, width = 1100))
+                            )
+                        ),
                 
                 tabItem(tabName = "redes",h2("Contraste de Hipotesis Redes Neuronales")),
                 
